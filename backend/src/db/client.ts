d3ae77f6491b426feea466fa9ec548ffe85c3a06
chandan-1427 <chandandakka@gmail.com>
2026-01-29 23:30:48 +0530

    Try1: Pool update

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,7 +1,25 @@
-import { Pool } from "pg";
+import pg, { type Pool } from "pg";
 import { env } from "../config/env.js";
 
-export const pool = new Pool({
-  connectionString: env.DATABASE_URL,
-  ssl: false,
-});
+declare global {
+  // eslint-disable-next-line no-var
+  var pgPool: Pool | undefined;
+}
+
+const isProduction = env.NODE_ENV === "production";
+
+export const pool =
+  global.pgPool ??
+  new pg.Pool({
+    connectionString: env.DATABASE_URL,
+    ssl: isProduction
+      ? { rejectUnauthorized: false }
+      : false,
+    max: 5,
+    idleTimeoutMillis: 30_000,
+    connectionTimeoutMillis: 2_000,
+  });
+
+if (!global.pgPool) {
+  global.pgPool = pool;
+}
